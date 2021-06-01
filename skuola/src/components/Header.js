555c0031eefbae2ea86b4c35e8c0af6bf62d2a4d
Tommaso Sangiorgio <sangiorgiota@gmail.com>
2021-06-01 10:37:09 +0200

    form converted into a component

--- skuola/src/components/Header.js
+++ skuola/src/components/Header.js
@@ -1,27 +1,16 @@
 import React, { useState } from 'react';
-import { Button, Modal, InputGroup, FormControl } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
+import Form from './Form';
 
 function Header() {
+
+    // modal 
 
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-
-    const submitUser = () => handleClose();
-
-
-    const anno = (new Date()).getFullYear();
-    const anni = Array.from(new Array(90), (val, index) => anno - index);
-    const giorni = Array.from(new Array(31), (val, index) => index + 1);
-    const mesi = Array.from(new Array(12), (val, index) => index + 1);
-    const date = {
-        giorni: giorni,
-        mesi: mesi,
-        anni: anni
-    }
-
-
+    console.log(localStorage);
 
     return (
         <div className="Header">
@@ -34,62 +23,13 @@ function Header() {
                         </Modal.Header>
 
                         <Modal.Body>
-                            <InputGroup className="mb-3">
-                                <FormControl
-                                    placeholder="Nome"
-                                    aria-label="Nome"
-                                    id="nome"
-                                    aria-describedby="basic-addon1"
-                                />
-                                <FormControl
-                                    placeholder="Cognome"
-                                    aria-label="Cognome"
-                                    id="cognome"
-                                    aria-describedby="basic-addon1"
-                                />
-                            </InputGroup>
-                            <label htmlFor="data">Data di nascita</label>
-                            <InputGroup id="data" className="mb-3">
-                                <FormControl className="form-control" id="giorno" as="select" custom>
-                                    <option value="" disabled="disabled" selected>Giorno</option>
-                                    {date.giorni.map(giorno => (
-                                        <option>{giorno}</option>
-                                    ))}
-                                </FormControl>
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text>/</InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <FormControl id="mese" className="form-control" as="select" custom>
-                                    <option value="" disabled="disabled" selected>Mese</option>
-                                    {date.mesi.map(mese => (
-                                        <option>{mese}</option>
-                                    ))}
-                                </FormControl>
-                                <InputGroup.Append>
-                                    <InputGroup.Text>/</InputGroup.Text>
-                                </InputGroup.Append>
-                                <FormControl id="anno" className="form-control" as="select" custom>
-                                    <option value="" disabled="disabled" selected>Anno</option>
-                                    {date.anni.map(anno => (
-                                        <option>{anno}</option>
-                                    ))}
-                                </FormControl>
-                            </InputGroup>
-                            <InputGroup className="mb-3">
-                                <FormControl
-                                    placeholder="Email"
-                                    aria-label="email"
-                                    aria-describedby="email"
-                                    id="email"
-                                    type="email"
-                                />
-                            </InputGroup>
+                            <Form />
                         </Modal.Body>
 
                         <Modal.Footer>
-                            <Button onClick={submitUser} variant="secondary">Annulla</Button>
-                            <Button onClick={handleClose} variant="primary">Aggiungi utente</Button>
+                            <Button onClick={handleClose} variant="secondary">Annulla</Button>
                         </Modal.Footer>
+
                     </Modal>
                 </div>
             </header>
